32e672739f59ede93510863d43cdb11c97ddac28
thiago <thiagoschaves61@gmail.com>
2023-12-06 11:44:56 -0300

    give some styles

--- frontend/src/authPage.js
+++ frontend/src/authPage.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
+import PropTypes from 'prop-types';
 
 const AuthPage = (props) => {
-  const [username, setUsername] = useState();
-  const [secret, setSecret] = useState();
-  const [email, setEmail] = useState();
-  const [first_name, setFirstName] = useState();
-  const [last_name, setLastName] = useState();
+  const [username, setUsername] = useState("");
+  const [secret, setSecret] = useState("");
+  const [email, setEmail] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
 
   const onLogin = (e) => {
     e.preventDefault();
     axios
       .post("http://localhost:3001/login", { username, secret })
-      .then((r) => props.onAuth({ ...r.data, secret })) // NOTE: over-ride secret
+      .then((r) => props.onAuth({ ...r.data, secret }))
       .catch((e) => console.log(JSON.stringify(e.response.data)));
   };
 
@@ -26,7 +27,7 @@ const AuthPage = (props) => {
         first_name,
         last_name,
       })
-      .then((r) => props.onAuth({ ...r.data, secret })) // NOTE: over-ride secret
+      .then((r) => props.onAuth({ ...r.data, secret }))
       .catch((e) => console.log(JSON.stringify(e.response.data)));
   };
 
@@ -40,15 +41,17 @@ const AuthPage = (props) => {
             type="text"
             name="username"
             placeholder="Username"
+            value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
             name="secret"
             placeholder="Password"
+            value={secret}
             onChange={(e) => setSecret(e.target.value)}
           />
-          <button type="submit">LOG IN</button>
+          <button type="submit" className="login-button">LOG IN</button>
         </form>
 
         {/* Sign Up Form */}
@@ -58,45 +61,93 @@ const AuthPage = (props) => {
             type="text"
             name="username"
             placeholder="Username"
+            value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
             name="secret"
             placeholder="Password"
+            value={secret}
             onChange={(e) => setSecret(e.target.value)}
           />
           <input
             type="text"
             name="email"
             placeholder="Email"
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="text"
             name="first_name"
             placeholder="First name"
+            value={first_name}
             onChange={(e) => setFirstName(e.target.value)}
           />
           <input
             type="text"
             name="last_name"
             placeholder="Last name"
+            value={last_name}
             onChange={(e) => setLastName(e.target.value)}
           />
-          <button type="submit">SIGN UP</button>
+          <button type="submit" className="signup-button">SIGN UP</button>
         </form>
       </div>
 
       <style>{`
-      .login-page { width: 100vw; height: 100vh; padding-top: 6vw; background: linear-gradient(180deg, rgba(117,84,160,1) 7%, rgba(117,84,160,1) 17%, rgba(106,95,168,1) 29%, rgba(99,103,174,1) 44%, rgba(87,116,184,1) 66%, rgba(70,135,198,1) 83%, rgba(44,163,219,1) 96%, rgba(22,188,237,1) 100%, rgba(0,212,255,1) 100%); }
-      .card { width: 200px; position: relative; left: calc(50vw - 100px); text-align: center; }
-      .title { padding-top: 32px; font-size: 22px; color: white; font-weight: 700; }
-      input { width: calc(100% - 16px); margin-top: 12px; padding: 8px; background-color: #e6f7ff; outline: none; border: 1px solid #e6f7ff; }
-      button { margin-top: 12px; width: 100%; padding: 8px; }
+        .login-page {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+          background: url('/background.jpg') center/cover;
+        }
+        .card {
+          width: 300px;
+          text-align: center;
+          background-color: rgba(255, 255, 255, 0.8);
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        }
+        .title {
+          font-size: 24px;
+          color: #333;
+          font-weight: 700;
+          margin-bottom: 20px;
+        }
+        input {
+          width: calc(100% - 20px);
+          margin-top: 12px;
+          padding: 10px;
+          background-color: #e6f7ff;
+          border: 1px solid #e6f7ff;
+          border-radius: 5px;
+          outline: none;
+          box-sizing: border-box;
+        }
+        button {
+          margin-top: 12px;
+          width: 100%;
+          padding: 10px;
+          background-color: #4caf50;
+          color: #fff;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+        }
+        .login-button {
+          background-color: #2196f3;
+        }
       `}</style>
     </div>
   );
+};
+
+AuthPage.propTypes = {
+  onAuth: PropTypes.func.isRequired,
 };
 
 export default AuthPage;
