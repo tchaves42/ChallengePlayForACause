45eef1941c76aa77345885bcc11a6914478d9eed
thiago <thiagoschaves61@gmail.com>
2023-12-06 12:23:54 -0300

    finishing the styles

--- frontend/src/authPage.js
+++ frontend/src/authPage.js
@@ -3,17 +3,17 @@ import axios from "axios";
 import PropTypes from 'prop-types';
 
 const AuthPage = (props) => {
-  const [username, setUsername] = useState("");
-  const [secret, setSecret] = useState("");
-  const [email, setEmail] = useState("");
-  const [first_name, setFirstName] = useState("");
-  const [last_name, setLastName] = useState("");
+  const [username, setUsername] = useState();
+  const [secret, setSecret] = useState();
+  const [email, setEmail] = useState();
+  const [first_name, setFirstName] = useState();
+  const [last_name, setLastName] = useState();
 
   const onLogin = (e) => {
     e.preventDefault();
     axios
       .post("http://localhost:3001/login", { username, secret })
-      .then((r) => props.onAuth({ ...r.data, secret }))
+      .then((r) => props.onAuth({ ...r.data, secret })) // NOTE: over-ride secret
       .catch((e) => console.log(JSON.stringify(e.response.data)));
   };
 
@@ -27,7 +27,7 @@ const AuthPage = (props) => {
         first_name,
         last_name,
       })
-      .then((r) => props.onAuth({ ...r.data, secret }))
+      .then((r) => props.onAuth({ ...r.data, secret })) // NOTE: over-ride secret
       .catch((e) => console.log(JSON.stringify(e.response.data)));
   };
 
@@ -40,108 +40,107 @@ const AuthPage = (props) => {
           <input
             type="text"
             name="username"
-            placeholder="Username"
-            value={username}
+            placeholder="Usuário"
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
             name="secret"
-            placeholder="Password"
-            value={secret}
+            placeholder="Senha"
             onChange={(e) => setSecret(e.target.value)}
           />
-          <button type="submit" className="login-button">LOG IN</button>
+          <button type="submit" className="login-button">Entrar</button>
         </form>
 
         {/* Sign Up Form */}
         <form onSubmit={onSignup}>
-          <div className="title">or Sign Up</div>
+          <div className="title">ou Cadastre-se</div>
           <input
             type="text"
             name="username"
-            placeholder="Username"
-            value={username}
+            placeholder="Usuário"
             onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
             name="secret"
-            placeholder="Password"
-            value={secret}
+            placeholder="Senha"
             onChange={(e) => setSecret(e.target.value)}
           />
           <input
             type="text"
             name="email"
             placeholder="Email"
-            value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="text"
             name="first_name"
-            placeholder="First name"
-            value={first_name}
+            placeholder="Nome"
             onChange={(e) => setFirstName(e.target.value)}
           />
           <input
             type="text"
             name="last_name"
-            placeholder="Last name"
-            value={last_name}
+            placeholder="Sobrenome"
             onChange={(e) => setLastName(e.target.value)}
           />
-          <button type="submit" className="signup-button">SIGN UP</button>
+          <button type="submit">Cadastre-se</button>
         </form>
       </div>
 
       <style>{`
-        .login-page {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          height: 100vh;
-          background: url('/background.jpg') center/cover;
-        }
-        .card {
-          width: 300px;
-          text-align: center;
-          background-color: rgba(255, 255, 255, 0.8);
-          padding: 20px;
-          border-radius: 10px;
-          box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
-        .title {
-          font-size: 24px;
-          color: #333;
-          font-weight: 700;
-          margin-bottom: 20px;
-        }
-        input {
-          width: calc(100% - 20px);
-          margin-top: 12px;
-          padding: 10px;
-          background-color: #e6f7ff;
-          border: 1px solid #e6f7ff;
-          border-radius: 5px;
-          outline: none;
-          box-sizing: border-box;
-        }
-        button {
-          margin-top: 12px;
-          width: 100%;
-          padding: 10px;
-          background-color: #4caf50;
-          color: #fff;
-          border: none;
-          border-radius: 5px;
-          cursor: pointer;
-        }
-        .login-button {
-          background-color: #2196f3;
-        }
-      `}</style>
+  .login-page {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background: url('/background.jpg') center/cover;
+  }
+
+  .card {
+    width: 300px;
+    text-align: center;
+    background-color: #000;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  }
+
+  .title {
+    font-size: 24px;
+    color: #fff;
+    font-weight: 700;
+    margin-bottom: 20px;
+  }
+
+  input {
+    width: calc(100% - 20px);
+    margin-top: 12px;
+    padding: 10px;
+    background-color: #00000;
+    border: 1px solid #e6f7ff;
+    border-radius: 5px;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  button {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    width: 100%;
+    padding: 10px;
+    background-color: #4caf50;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .login-button {
+    background-color: #2196f3;
+  }
+`}</style>
     </div>
   );
 };
--- frontend/src/chatsPage.js
+++ frontend/src/chatsPage.js
@@ -1,16 +1,26 @@
 import { PrettyChatWindow } from "react-chat-engine-pretty";
+import PropTypes from 'prop-types';
 
 const ChatsPage = (props) => {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <PrettyChatWindow
         projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-        username={props.user.username} // adam
-        secret={props.user.secret} // pass1234
+        username={props.user.username}
+        secret={props.user.secret}
         style={{ height: "100%" }}
       />
     </div>
   );
 };
 
+// Corrija para ChatsPage.propTypes
+ChatsPage.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    secret: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 export default ChatsPage;
+
